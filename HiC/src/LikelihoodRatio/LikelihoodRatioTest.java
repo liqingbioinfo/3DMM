936e39099b4edd5346ffc8f7d2f3ba4335be3800
liqingbioinfo <liqingbioinfo@gmail.com>
2021-01-29 10:55:56 -0700

    Add LikelihoodRatio test

---
+++ HiC/src/LikelihoodRatio/LikelihoodRatioTest.java
@@ -0,0 +1,14 @@
+package LikelihoodRatio;
+
+public class LikelihoodRatioTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if (args.length == 0) {
+			System.out.println("inputs: " + "outputs : " + "enter all above files path");
+		} else {
+		}
+
+	}
+
+}
